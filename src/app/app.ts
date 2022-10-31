5284bc7f96abbf8826ef97c564c3aa97e1936211
Anoop-LogCat <anoopbenzier1997@outlook.com>
2022-10-31 23:09:03 +0530

    :sparkles: feat(frontend): mouse based sphere moving

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,25 +2,28 @@ import {
   Scene,
   PerspectiveCamera,
   WebGLRenderer,
-  Mesh,
-  BoxGeometry,
-  MeshStandardMaterial,
   ACESFilmicToneMapping,
   sRGBEncoding,
-  PointLight,
   AmbientLight,
+  Raycaster,
+  Vector3,
+  Vector2,
+  Plane,
+  SphereGeometry,
+  MeshStandardMaterial,
+  Mesh,
 } from "three";
-import Cube from "./cube";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new Scene();
 
 const camera = new PerspectiveCamera(
-  75,
+  70,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.z = 5;
+camera.position.set(20, 20, 20);
 
 const renderer = new WebGLRenderer({ alpha: true, antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -28,22 +31,39 @@ renderer.toneMapping = ACESFilmicToneMapping;
 renderer.outputEncoding = sRGBEncoding;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-const cube = new Cube();
-scene.add(cube);
+scene.add(new AmbientLight(0x222222));
 
-const pointLight = new PointLight();
-pointLight.position.set(9, 9, 9);
-scene.add(pointLight);
+const geometry = new SphereGeometry(1, 12, 8);
+const material = new MeshStandardMaterial();
+const sphere = new Mesh(geometry, material);
+scene.add(sphere);
 
-const ambientLight = new AmbientLight();
-scene.add(ambientLight);
+document.body.addEventListener("mousemove", onMouseMoveHandler, false);
+window.addEventListener("resize", onWindowResize, false);
+const raycaster = new Raycaster();
+const plane = new Plane(new Vector3(0, 2, 0), 0);
+const mouse = new Vector2();
+const intersects = new Vector3();
+
+function onMouseMoveHandler(event) {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  raycaster.setFromCamera(mouse, camera);
+  raycaster.ray.intersectPlane(plane, intersects);
+  sphere.position.set(intersects.x, intersects.y, intersects.z);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-
-  cube.update();
 }
 
 animate();
